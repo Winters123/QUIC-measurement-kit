e713fc49478a12285c94540534ae66f8795970c5
18635723769@163.com <18635723769@163.com>
2020-07-08 13:54:09 +0100

    weird client

--- kits/quant/client_netmap.sh
+++ kits/quant/client_netmap.sh
@@ -1,9 +1,9 @@
 #!/bin/ bash
 # $a is the test tag (can be used to identify loss rate, etc.)
-a=$1
-cp server_netmap.sh ../../extern/quant/Release .
+cp client_netmap.sh ../../extern/quant/Release .
 cd ../../extern/quant/Release
 
+a=$1
 echo "+++{$a}" >> bps_ins.txt
 #change client-warp to client to replace netmap with socket.
 taskset 64 ./bin/client-warp -b 50000 -r 20 -i ens3f1 http://192.168.0.1:4433/50000000
