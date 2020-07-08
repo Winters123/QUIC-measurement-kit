3518102deb8088e0e9746f2d0885310309ff30cc
18635723769@163.com <18635723769@163.com>
2020-07-08 21:41:55 +0100

    change the eth interface name

--- kits/quant/client_netmap.sh
+++ kits/quant/client_netmap.sh
@@ -7,5 +7,5 @@ a=$1
 echo "+++{$a}" >> bps_ins.txt
 #change client-warp to client to replace netmap with socket.
 taskset 64 ./bin/client-warp -b 50000 -r 20 -i ens3f1 http://192.168.0.1:4433/50000000
-sleep(1)
+sleep 1
 echo "---{$a}" >> bps_ins.txt
--- kits/quant/server_netmap.sh
+++ kits/quant/server_netmap.sh
@@ -1,3 +1,3 @@
 #!/bin/ bash
 
-taskset 128 ip netns exec blue ./bin/server-warp -b 50000 -i ens3f1 -p 4433
+taskset 128 ip netns exec blue ./bin/server-warp -b 50000 -i ens3f0 -p 4433
