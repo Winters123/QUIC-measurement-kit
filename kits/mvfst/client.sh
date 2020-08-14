73f6a52fb9775e98a16307fbaf72fb7738716cdd
Yuecheng <43573044+yc1997427@users.noreply.github.com>
2020-08-14 13:55:50 +0100

    Update client.sh

--- kits/mvfst/client.sh
+++ kits/mvfst/client.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/ bash
 #a stands for total repetitions of test (duration)
-a = $1
+
 echo "+++{$a}" >> throughput.txt
-taskset 128 ./tperf -max_log_size 1 -congestion cubic -duration {$a} -host 192.168.0.1 -mode client -p 4433
+#taskset 128 ./tperf -max_log_size 1 -congestion cubic -duration {$a} -host 192.168.0.1 -mode client -p 4433
 #use the cmd below to fire up perf.
-#perf record --call-graph lbr -- taskset 128 ./tperf -max_log_size 1 -congestion cubic -duration {$a} -host 192.168.0.1 -mode client -p 4433
-sleep(1)
-echo "---{$a}" >> throughput.txt
+perf record --call-graph lbr -- taskset 128 ~/yuecheng/QUIC-measurement-kit/extern/mvfst/_build/build/quic/tools/tperf/./tperf -max_log_size 1 -congestion cubic -duration $1 -host 192.168.0.1 -mode client -port 4433
+sleep 1
+echo "---{$1}" >> throughput.txt
