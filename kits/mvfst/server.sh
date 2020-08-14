46944a0793b8cf65403cd762cf013a5f7b6abfd1
Yuecheng <43573044+yc1997427@users.noreply.github.com>
2020-08-14 13:56:23 +0100

    Update server.sh

--- kits/mvfst/server.sh
+++ kits/mvfst/server.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/ bash
 
-ip netns exec blue taskset 128 ./tperf -max_log_size 1 -congestion cubic -host 192.168.0.1 -mode server -p 4433
+#ip netns exec blue taskset 128 ~/yuecheng/QUIC-measurement-kit/extern/mvfst/_build/build/quic/tools/tperf/./tperf -max_log_size 1 -congestion cubic -host 192.168.0.2 -mode server -port 4433
 #use the cmd below to fire up perf.
-#perf record --call-graph lbr ip netns exec blue taskset 128 ./tperf -max_log_size 1 -congestion cubic -host 192.168.0.1 -mode server -p 4433
+perf record --call-graph lbr ip netns exec blue taskset 128 ~/yuecheng/QUIC-measurement-kit/extern/mvfst/_build/build/quic/tools/tperf/./tperf -max_log_size 1 -congestion cubic -host 192.168.0.1 -mode server -port 4434
